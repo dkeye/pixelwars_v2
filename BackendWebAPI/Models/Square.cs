2199335044adce2cdb6f7b967664816d024eb429
Cyb3rTr0ll3737 <65198423+Cyb3rTr0ll3737@users.noreply.github.com>
2022-11-24 00:52:04 +0300

    Подключить базу данных#26 (#34)

---
+++ BackendWebAPI/Models/Square.cs
@@ -0,0 +1,9 @@
+﻿namespace BackendWebAPI.Models
+{
+    public class Square
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+        public string color { get; set; } = null!;
+    }
+}
