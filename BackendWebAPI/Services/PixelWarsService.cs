3ee0cb60b94ecd20c6f4b9f3e0bf3e63e5f90b14
Марков Анатолий <TaJIuK@bk.ru>
2022-11-23 11:58:23 +0400

    variable name correction

--- BackendWebAPI/Services/PixelWarsService.cs
+++ BackendWebAPI/Services/PixelWarsService.cs
@@ -31,7 +31,7 @@ namespace BackendWebAPI.Services
         public async Task CreateAsync(PixelWarsCollection newTest) =>
             await _PixelWarsCollectionName.InsertOneAsync(newTest);
 
-        public async Task UpdateAsync(string id, PixelWarsCollection updatedTest) =>
-            await _PixelWarsCollectionName.ReplaceOneAsync(x => x.Id == id, updatedTest);
+        //public async Task UpdateAsync(string id, PixelWarsCollection updatedTest) =>
+        //    await _PixelWarsCollectionName.ReplaceOneAsync(x => x.Id == id, updatedTest);
     }
 }
