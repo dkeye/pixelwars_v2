1c6235913d2a7189e8d5ab88c15a20112de8ec49
Марков Анатолий <TaJIuK@bk.ru>
2022-12-16 17:42:26 +0400

    Create answer for requests

--- BackendWebAPI/Models/PixelWarsDB.cs
+++ BackendWebAPI/Models/PixelWarsDB.cs
@@ -1,20 +1,34 @@
 ﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization;
 using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson.Serialization.Serializers;
 using System.Text.Json.Serialization;
+using System.Web.Helpers;
 
 #nullable disable
 namespace BackendWebAPI.Models
 {
     public class PixelWarsCollection
     {
+        private Square square1;
+
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
 
-        [BsonElement("Name")]
-        [JsonPropertyName("Name")]
+        [BsonElement("Squares")]
+        [BsonRepresentation(BsonType.String)]
+        public string Squares { get; set; }       
 
-        public Square square { get; set; }
+
+        [BsonElement("x")]
+        [BsonRepresentation(BsonType.Int32)]
+        public int x { get; set; }
+
+        [BsonElement("Size")]
+        [BsonRepresentation(BsonType.String)]
         public string Size { get; set; }
+
+
     }
 }
