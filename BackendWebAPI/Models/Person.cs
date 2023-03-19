9d560c6da0e2dd390a57ca8d07de7c5f2095b63a
Cyb3rTr0ll3737 <TaJIuK@bk.ru>
2023-03-20 02:14:11 +0400

    Add cookie

---
+++ BackendWebAPI/Models/Person.cs
@@ -0,0 +1,16 @@
+﻿namespace BackendWebAPI.Models
+{
+    public class Person
+    {
+        public Person(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+
+    }
+}
--- BackendWebAPI/Program.cs
+++ BackendWebAPI/Program.cs
@@ -1,7 +1,16 @@
-using BackendWebAPI.Models;
+﻿using BackendWebAPI.Models;
 using BackendWebAPI.Services;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Authentication;
+using System;
 
+// условная бд с пользователями
+var people = new List<Person>
+{
+    new Person("tom", "tom")
+};
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,13 +18,77 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.Configure<PixelWarsDatabaseSettings>(builder.Configuration.GetSection("PixelWarsDatabase"));
 builder.Services.AddSingleton<PixelWarsService>();
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
+// аутентификация с помощью куки
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options => options.LoginPath = "/login");
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
-app.UseStaticFiles();
+
+app.MapGet("/login", async (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    // html-форма для ввода логина/пароля
+    string loginForm = @"<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='utf-8' />
+        <title>METANIT.COM</title>
+    </head>
+    <body>
+        <h2>Login Form</h2>
+        <form method='post'>
+            <p>
+                <label>Email</label><br />
+                <input name='email' />
+            </p>
+            <p>
+                <label>Password</label><br />
+                <input type='password' name='password' />
+            </p>
+            <input type='submit' value='Login' />
+        </form>
+    </body>
+    </html>";
+    await context.Response.WriteAsync(loginForm);
+});
+
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    // получаем из формы email и пароль
+    var form = context.Request.Form;
+    // если email и/или пароль не установлены, посылаем статусный код ошибки 400
+    if (!form.ContainsKey("email") || !form.ContainsKey("password"))
+        return Results.BadRequest("Email и/или пароль не установлены");
+
+    string email = form["email"];
+    string password = form["password"];
+
+    // находим пользователя 
+    Person? person = people.FirstOrDefault(p => p.Email == email && p.Password == password);
+    // если пользователь не найден, отправляем статусный код 401
+    if (person is null) return Results.Unauthorized();
+
+    var claims = new List<Claim> { new Claim(ClaimTypes.Name, person.Email) };
+    // создаем объект ClaimsIdentity
+    ClaimsIdentity claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    // установка аутентификационных куки
+    await context.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+    return Results.Redirect(returnUrl?? "/");
+});
+
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return Results.Redirect("/login");
+});
+
+app.Map("/", [Authorize] () => $"Hello World!");
+
+
 
 // Configure the HTTP request pipeline.t
 if (app.Environment.IsDevelopment())
@@ -24,10 +97,10 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseStaticFiles();
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
 
 app.Run();
+
