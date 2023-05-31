11206eb04999fce879a69b148ede33422bceaf88
Cyb3rTr0ll3737 <TaJIuK@bk.ru>
2023-05-31 20:11:33 +0400

    Improve InsertGrid method

--- BackendWebAPI/Models/PixelWarsDB.cs
+++ BackendWebAPI/Models/PixelWarsDB.cs
@@ -1,16 +1,22 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using System.ComponentModel.DataAnnotations.Schema;
 
 #nullable disable
 namespace BackendWebAPI.Models
 {
-    public class PixelWarsCollection
+    public class PixelWarsGrid
     {
         [BsonId]
         [BsonRepresentation(BsonType.ObjectId)]
         public string Id { get; set; }
         public string Name { get; set; }
-        public Square[] Squares { get; set; }       
+        public Square[] Squares { get; set; }
+
+        [BsonIgnore]
+        public uint x { get; set; }
+        [BsonIgnore]
+        public uint y { get; set; }
 
         [BsonElement("Size")]
         [BsonRepresentation(BsonType.String)]
